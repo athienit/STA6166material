d513238cac0997a8f6728c9cbf46c0fcc24a12d1
Demetris Athienitis <56597686+athienit@users.noreply.github.com>
2020-05-06 12:17:01 -0400

    Update hist1_boxplot1.R

--- hist1_boxplot1.R
+++ hist1_boxplot1.R
@@ -6,9 +6,21 @@ library(ggplot2)
 ggplot(data.frame(frac),aes(x=frac))+geom_dotplot(binwidth=20) #binwidth=1/30*range default
 
 #make non-fancy histogram
-hist(frac,nclass=6,freq=TRUE,main="Histogram",xlab="Fracture stress") # plots using frequency
+hist(frac,nclass=6,freq=FALSE,main="Histogram",xlab="Fracture stress") # plots using frequency
 hist(frac,breaks=c(0,100,200,300,500),freq=TRUE,main="Histogram",xlab="Fracture stress")
 hist(frac,breaks=c(0,100,200,300,500),freq=FALSE,main="Histogram",xlab="Fracture stress") #plots using density
+
+#somewhat fancy histogram
+ggplot(data=data.frame(frac), aes(frac)) + 
+  geom_histogram(aes(y =..density..), 
+                 bins=5,
+                 binwidth=100,
+                 center=50,
+                 col="red", 
+                 fill="green", 
+                 alpha=.2)+ 
+  geom_density(col=2)+
+  labs(title="Histogram", x="Fracture Stress", y="Density")
 
 #make non-fancy boxplot
 boxplot(frac,horizontal=TRUE,xlab="Fracture stress")
