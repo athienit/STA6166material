d64ab65df0b9dd7068b5475a6838a5ca884da695
clas-adm-demetris <56597686+athienit@users.noreply.github.com>
2022-05-18 20:36:58 -0400

    Update CLT_illustration.R

--- CLT_illustration.R
+++ CLT_illustration.R
@@ -1,11 +1,8 @@
-par(mfrow=c(2,2))
+set.seed(192)
+ns=100 # sample size of each sample
+k=250 # collection of sample means to draw histogram
 
-set.seed(1925) #221186
-n=c(5,20,100,500) #number of samples
-sn=100 #sample size of each sample
-
-for(i in 1:length(n)){
-x=array(runif(ns*n[i],0,50),dim=c(n[i],sn)) #sample from 
+x=array(runif(ns*k,0,50),dim=c(k,sn)) #sample from 
 means=apply(x,1,mean)
 
 hist(means, # histogram
@@ -13,8 +10,7 @@ hist(means, # histogram
      border="black",
      prob = TRUE, # show densities instead of frequencies
      xlab = "means",
-     main = paste(c("no. of samples is "),n[i]))
+     main = paste(c("no. of samples is "),k))
 lines(density(means,bw=0.5), # density plot
       lwd = 2, # thickness of line
       col = "red")
-}
