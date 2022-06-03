676ac62b02594bd2319c346336bae90b5a90f662
clas-adm-demetris <56597686+athienit@users.noreply.github.com>
2022-06-03 07:59:15 -0400

    Update wilcox_1.R

--- wilcox_1.R
+++ wilcox_1.R
@@ -7,8 +7,28 @@ T=sum(rank(c(placebo,alcohol))[1:length(placebo)]);T
 #check assumption that centered data have the same distribution
 pcenter=placebo-mean(placebo)
 acenter=alcohol-mean(alcohol)
-qqplot(pcenter,acenter,main="QQ plot of centered data",ylab="Centered alcohol quantiles",xlab="Centered placebo quantiles")
-abline(c(0,1))
+# basic plot
+plot(density(pcenter),main="Histograms of centered values")
+lines(density(acenter),col=2,lty=2,add=TRUE)
+legend(0.5,1.5,legend=c("Placebo","Alcohol"),col=1:2,lty=1:2,bg="lightgrey")
+
+# fancy plot
+library(ggplot2)
+library(reshape2)
+# prepare data in long format
+lf=melt(cbind(pcenter,acenter))
+lf=lf[,-1]
+colnames(lf)=c("Treatment","Value")
+
+ggplot(lf, aes(x=Value, fill=Treatment))+
+  geom_histogram(bins=15,color="grey80")+
+  geom_density(alpha=.2, fill="grey") +
+  facet_grid(Treatment~.)
+
+ggplot(lf, aes(x=Value, fill=Treatment, color=Treatment)) +
+  geom_histogram(bins=15,position="identity", alpha=0.25)
+
+
 ks.test(pcenter,acenter)
 
 wilcox.test(placebo,alcohol,conf.int=TRUE)
